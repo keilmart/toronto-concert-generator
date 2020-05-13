8369b02fe1e1d471e23c705afa2631436a3b8ce7
keil <keilwmartin@gmail.com>
2020-05-13 12:21:25 -0400

    Updates

---
+++ precache-manifest.eda98d9838d6b3d3c5ff3e0a3fd4fed7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1148634cd3fcd7745b85ac85c7e2a9e4",
+    "url": "/Keil-Martin-Project-5/index.html"
+  },
+  {
+    "revision": "6a89b80664f42060b030",
+    "url": "/Keil-Martin-Project-5/static/css/main.5b6d3fa0.chunk.css"
+  },
+  {
+    "revision": "b6ca188cedb829f1a1ac",
+    "url": "/Keil-Martin-Project-5/static/js/2.cff5a3c6.chunk.js"
+  },
+  {
+    "revision": "6b1c9aa1f24077e4bfd4b51db2988c14",
+    "url": "/Keil-Martin-Project-5/static/js/2.cff5a3c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a89b80664f42060b030",
+    "url": "/Keil-Martin-Project-5/static/js/main.bdecd284.chunk.js"
+  },
+  {
+    "revision": "7c94fa72d59131928630",
+    "url": "/Keil-Martin-Project-5/static/js/runtime-main.5e146ff6.js"
+  },
+  {
+    "revision": "f57aa581359652b8abb6c0057acd88a4",
+    "url": "/Keil-Martin-Project-5/static/media/rapper.f57aa581.jpg"
+  },
+  {
+    "revision": "fec9fb6835d16e84eda66bab796ea2cb",
+    "url": "/Keil-Martin-Project-5/static/media/stageHeader.fec9fb68.jpg"
+  }
+]);
